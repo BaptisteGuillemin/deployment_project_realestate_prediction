aeb355b82631a6aa2a7ac3a987980d4bb0cc08aa
Baptiste Guilllemin <baptiste.guillemin@aivancity.education>
2023-03-09 14:33:36 +0100

    1st commit

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 # Importing the libraries
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -6,8 +7,7 @@ import pickle
 
 # Simple dataset of three four features 
 # First of all load csv dataset file into dataframe
-dataset = pd.read_csv(?)
-
+dataset = pd.read_csv(Hiring.csv)
 #print the dataset
 
 # feature engineering 
@@ -17,7 +17,7 @@ dataset['experience'].fillna(0, inplace=True)
 dataset['test_score'].fillna(dataset['test_score'].mean(), inplace=True)
 
 # now specify the Input features for training 
-X = dataset.iloc[]
+X = dataset.iloc[:, :-1]
 
 # Now some features are string/text, we need to convert them to number
 #Converting words to integer values
@@ -28,7 +28,7 @@ def convert_to_int(word):
 
 X['experience'] = X['experience'].apply(lambda x : convert_to_int(x))
 
-y = ?
+y = dataset.iloc[:, -1]
 
 #Splitting Training and Test Set
 #Since we have a very small dataset, we will train our model with all availabe data.
@@ -39,7 +39,7 @@ from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 
 #Fitting model with trainig data
-regressor.?
+regressor.fit(X, y)
 
 # Saving model to disk
 pickle.dump(regressor, open('model.pkl','wb'))
@@ -48,4 +48,4 @@ pickle.dump(regressor, open('model.pkl','wb'))
 model = pickle.load(open('model.pkl','rb'))
 print("Test one prediction")
 # using predict() function to test your model 
-# your code here 
+print(model.predict(([7, 9, 6])))
